0e79550965827c9eeb864b701268948ce8d65a5f
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-06-03 18:32:39 +0200

    hyperparameters tuned

--- gazebo/ArmPlugin.cpp
+++ gazebo/ArmPlugin.cpp
@@ -39,8 +39,8 @@
 #define INPUT_WIDTH 64
 #define INPUT_HEIGHT 64
 #define NUMBER_OF_ACTIONS 2 * DOF
-#define OPTIMIZER "Adam"
-#define LEARNING_RATE 0.05f
+#define OPTIMIZER "RMSprop"
+#define LEARNING_RATE 0.01f
 #define REPLAY_MEMORY 20000
 #define BATCH_SIZE 512
 #define USE_LSTM true
